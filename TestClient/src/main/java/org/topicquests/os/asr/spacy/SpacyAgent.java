6b7d01474953474bec101a73f7b6e3b73e90ddee
Jack Park <jackpark@gmail.com>
2020-10-01 11:05:14 -0700

    Adapted client to latest server

--- TestClient/src/main/java/org/topicquests/os/asr/spacy/SpacyAgent.java
+++ TestClient/src/main/java/org/topicquests/os/asr/spacy/SpacyAgent.java
@@ -3,6 +3,9 @@
  */
 package org.topicquests.os.asr.spacy;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.topicquests.os.asr.spacy.api.ISpacyAgent;
 import org.topicquests.support.ResultPojo;
 import org.topicquests.support.api.IResult;
@@ -17,6 +20,7 @@ import net.minidev.json.parser.JSONParser;
 public class SpacyAgent implements ISpacyAgent {
 	private SpacyClientEnvironment environment;
 	private HttpClient http;
+	private final List<String> models;
 	private final String
 		SERVER,
 		PORT,
@@ -28,13 +32,24 @@ public class SpacyAgent implements ISpacyAgent {
 	public SpacyAgent(SpacyClientEnvironment env, HttpClient cl) {
 		environment = env;
 		http = cl;
+		models = new ArrayList<String>();
+		models.add("en_ner_jnlpba_md");
+		models.add("en_ner_bc5cdr_md");
+		models.add("en_ner_bionlp13cg_md");
+		models.add("en_ner_craft_md");
+		models.add("en_core_web_lg");
+		models.add("en_core_sci_lg");
 		SERVER = environment.getStringProperty("ServerURl");
 		PORT = environment.getStringProperty("ServerPort");
-		URL = "http://"+SERVER+":"+Integer.parseInt(PORT)+"/analyze/";
+		URL = "http://"+SERVER+":"+Integer.parseInt(PORT)+"/all";
 	}
 
 	@Override
 	public IResult processSentence(JSONObject sentence) {
+
+
+		sentence.put("models", models);
+
 		IResult result = new ResultPojo();
 		result.setResultObject(sentence);
 		String text = sentence.toJSONString();
@@ -49,6 +64,8 @@ public class SpacyAgent implements ISpacyAgent {
 				JSONParser p = new JSONParser(JSONParser.MODE_JSON_SIMPLE);
 				JSONObject jo = (JSONObject)p.parse(json);
 				sentence.put("results", jo);
+				sentence.remove("models");
+
 			} catch (Exception e) {
 				e.printStackTrace();
 				result.addErrorString(e.getLocalizedMessage());
--- TestClient/src/main/java/org/topicquests/os/asr/spacy/api/ISpacyAgent.java
+++ TestClient/src/main/java/org/topicquests/os/asr/spacy/api/ISpacyAgent.java
@@ -19,9 +19,12 @@ public interface ISpacyAgent {
 	 * {@code JSONObject} which includes the results of all
 	 * spaCy models applied to the give sentence.</p>
 	 * <p>{@code sentence} is of the form:<br/>
-	 * 		{ "sentenceId":"<some id>", "text":"<sentence text>" }</p>
+	 * 		{ "sentenceId": <someId>, "text":"<sentence text>" }</p>
 	 * @param sentence
 	 * @return
 	 */
-	IResult processSentence(JSONObject sentence);
+	IResult processSentence(JSONObject  sentence);
 }
+/**
+{"models":["en_ner_jnlpba_md","en_ner_bc5cdr_md","en_ner_bionlp13cg_md","en_ner_craft_md","en_core_web_lg","en_core_sci_lg"],"text":"CO2 causes climate change"}' 
+*/
